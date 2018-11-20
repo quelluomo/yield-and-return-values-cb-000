5fe970d44d281283e7b7c8e945b865d0eb7d1b14
Matteo Muscas <thatman.j@gmail.com>
2018-11-20 11:20:53 +0000

    automatically backed up by learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 def hello(array)
   i = 0
   collection = []
@@ -7,6 +5,7 @@ def hello(array)
     collection << yield(array[i])
     i += 1
   end
+  collection
 end
 
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
